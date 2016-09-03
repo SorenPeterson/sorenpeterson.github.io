ccec2476a23f648a83972c04339d43fa719ba7c0
Soren Peterson <soren468@gmail.com>
2016-09-02 19:45:09 -0500

    add most of the configurations for development

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,25 @@
 var HtmlWebpackPlugin = require('html-webpack-plugin');
+
 var webpackConfig = {
     entry: './src/index.js',
     output: {
         filename: 'bundle.js'
     },
-    plugins: [new HtmlWebpackPlugin()]
+    module: {
+        loaders: [
+            {
+                test: /\.jsx?/,
+                loader: 'babel',
+                exclude: /node_modules/,
+                query: {
+                    presets: ['es2015', 'react']
+                }
+            }
+        ]
+    },
+    plugins: [new HtmlWebpackPlugin({
+        template: 'src/html/index.html'
+    })]
 };
+
 module.exports = webpackConfig;
