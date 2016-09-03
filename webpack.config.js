0f2370b8f74fa81c304dd2c93b1990738d6769ed
Soren Peterson <soren468@gmail.com>
2016-09-02 19:18:40 -0500

    recompile into the root folder

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,6 @@ var HtmlWebpackPlugin = require('html-webpack-plugin');
 var webpackConfig = {
     entry: './src/index.js',
     output: {
-        path: 'dist',
         filename: 'bundle.js'
     },
     plugins: [new HtmlWebpackPlugin()]
